be099131c6a6f3b6e9b8a8473977c94bdda1bc5e
Dillon Marquard <dmarquard4@gmail.com>
2024-09-10 17:18:18 -0700

    remove lib since moving web binary to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 pub mod ndbc;
 
 use actix_web::{get, web, App, HttpServer, Responder};
-use log::info;
+use log::{info, warn};
 use ndbc::{
     historic::get_station_historical_stdmet_data,
     ndbc_schema::Station,
@@ -12,6 +12,10 @@ use ndbc::{
 async fn service_active_stations() -> Result<impl Responder, Box<dyn std::error::Error>> {
     info!("service_active_stations");
     let active_stations = get_active_stations().await?;
+
+    if active_stations.is_empty() {
+        warn!("No active stations were found");
+    }
 
     Ok(web::Json(active_stations))
 }
@@ -25,6 +29,10 @@ async fn service_active_stdmet_stations() -> Result<impl Responder, Box<dyn std:
         .filter(|s| s.met.is_some_and(|x| x))
         .collect();
 
+    if active_stdmet_stations.is_empty() {
+        warn!("No active stdmet stations were found");
+    }
+
     Ok(web::Json(active_stdmet_stations))
 }
 
@@ -36,6 +44,10 @@ async fn service_station_stdmet_realtime_data(
     let id = path.into_inner();
     let res = get_station_realtime_stdmet_data(&id).await?;
 
+    if res.is_empty() {
+        warn!("No realtime stdmet data was found for the station: {id}");
+    }
+
     Ok(web::Json(res))
 }
 
@@ -46,6 +58,10 @@ async fn service_station_stdmet_historic_data(
     info!("service_station_stdmet_historic_data");
     let (id, year) = path.into_inner();
     let res = get_station_historical_stdmet_data(&id, &year).await?;
+
+    if res.is_empty() {
+        warn!("No stdmet data was found for the station: {id} for the year of {year}");
+    }
 
     Ok(web::Json(res))
 }
