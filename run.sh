085c7f8d7963e6be06a231175d4d2d1a0cc53a2b
dillonmarquard <dmarquard4@gmail.com>
2024-09-27 03:19:28 -0700

    delete shell script

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-k3d cluster delete demo-cluster
-docker build -t rust-service-ndbc-noaa .
-k3d cluster create --api-port 6550 -p "8081:80@loadbalancer" --agents 2 --wait demo-cluster
-k3d image import rust-service-ndbc-noaa -c demo-cluster
-kubectl apply -f deployment.yaml
